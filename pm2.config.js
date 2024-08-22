d5c32f8519b6a423ac8ac3d18d9e87d604f79df4
zohaib <apricopt@gmail.com>
2024-08-22 22:01:55 +0500

    Pm2 config added

---
+++ pm2.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+      {
+        name: "Abby Bot",     
+        script: "./server.js",  
+        instances: 1,        
+        watch: false, 
+      }
+    ]
+  };
